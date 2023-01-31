bbc927128e731954a8e3387d9dee83b6f5d94314
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2023-01-31 14:58:31 +0100

    add navigation rail

--- lib/src/features/home/presentation/home.page.dart
+++ lib/src/features/home/presentation/home.page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gun_club/src/core/constants/supabase.constants.dart';
 
 // TODO: Build real home page and delete prototype
 class HomePage extends StatelessWidget {
@@ -8,11 +7,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
+      body: Row(
         children: [
-          const Text("Home Page"),
-          Text(supabase.auth.currentUser?.email ?? "no user"),
+          NavigationRail(
+            selectedIndex: 0,
+            onDestinationSelected: (int index) {},
+            labelType: NavigationRailLabelType.all,
+            trailing: IconButton(
+              icon: const Icon(Icons.logout_outlined, color: Colors.red),
+              onPressed: () {},
+            ),
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.person_outline_outlined),
+                selectedIcon: Icon(Icons.person),
+                label: Text('Profile'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.calendar_today_outlined),
+                selectedIcon: Icon(Icons.calendar_today),
+                label: Text('Kalender'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.description_outlined),
+                selectedIcon: Icon(Icons.description),
+                label: Text('Lizenzen'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.flag_outlined),
+                selectedIcon: Icon(Icons.flag),
+                label: Text('Report'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.stacked_line_chart_outlined),
+                selectedIcon: Icon(Icons.stacked_line_chart),
+                label: Text('Statistik'),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          const Expanded(
+            child: Center(
+              child: Text('Rail Content'),
+            ),
+          )
         ],
       ),
     );
