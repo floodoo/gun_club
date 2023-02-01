2d68bf8150f531263aba0c6b1485554ea01af4a6
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2023-02-01 19:55:28 +0100

    fix home navigation

--- lib/src/features/start/presentation/start.page.dart
+++ lib/src/features/start/presentation/start.page.dart
@@ -6,12 +6,18 @@ import 'package:gun_club/src/core/user/controller/user.controller.dart';
 class StartPage extends ConsumerWidget {
   const StartPage({super.key});
 
+  Future<void> navigateToHome(BuildContext context) async {
+    final goRouter = GoRouter.of(context);
+    await Future.delayed(Duration.zero);
+    goRouter.go('/');
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: ref.watch(userControllerProvider).when(
             data: (user) {
-              context.go('/');
+              navigateToHome(context);
               return const CircularProgressIndicator();
             },
             loading: () => const CircularProgressIndicator(),
