b32ba19ba4829dafe1f3176ed3c6874aae1fafaa
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2023-02-01 19:58:52 +0100

    change folder structure

--- lib/router.dart
+++ lib/router.dart
@@ -4,8 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gun_club/src/core/constants/supabase.constants.dart';
 import 'package:gun_club/src/core/ui/widgets/web_layout.dart';
-import 'package:gun_club/src/features/account/presentation/login/login.page.dart';
-import 'package:gun_club/src/features/start/presentation/start.page.dart';
+import 'package:gun_club/src/features/account/presentation/login.page.dart';
+import 'package:gun_club/src/core/ui/widgets/start.page.dart';
 
 final routerProvider = Provider<GoRouter>((ref) => _goRouter);
 final _goRouter = GoRouter(
